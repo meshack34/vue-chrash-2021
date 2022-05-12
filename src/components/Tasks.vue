d246f91e9830be8496c362cfa096fe658802a584
meshack34 <meshackkimutai34@gmail.com>
2022-05-12 15:07:44 +0300

    Array

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,33 +1,17 @@
 <template>
-
+  <div :key="task.id" v-for="task in tasks">
+   <h3>{{task.text}}</h3>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: 'Task',
+  name: 'Tasks',
   props: {
-    task: Array,
-  },
+    tasks: Array, 
+  }, 
 }
 </script>
 
-<style scope>
-.fas {
-  color: red;
-}
-.task {
-  background: #f4f4f4;
-  margin: 5px;
-  padding: 10px 20px;
-  cursor: pointer;
-}
-.task.reminder {
-  border-left: 5px solid green;
-}
-.task h3 {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-</style>
+
